9c7798a17229efc15b755ab0c32508ad26ec61ba
bpederse <bpederse@5ce9a412-225a-11df-b218-1554cd8eeda6>
2010-03-23 16:48:45 +0000

    re-run queries to show speed after indexes are built.

--- tcapi_test.py
+++ tcapi_test.py
@@ -9,7 +9,7 @@ if os.path.exists(datadir):
     import shutil
     shutil.rmtree(datadir)
 
-nvals = int(2 * 1e6)
+nvals = int(3 * 1e6)
 
 conditions = ("a<5", "a+a>150", "a < 60 and c < 60", "c > 90", "c > a")
 counts = (5, nvals - 76, 19, 10, 50)
@@ -29,16 +29,24 @@ mult = fb.rows_in_partition(datadir)
 if mult != nvals:
     print 'Directory %d contains %d rows, expected %i' % (datadir, mult, nvals)
     sys.exit(1)
+#FastBit.build_index(datadir, 'a', '0')
+#FastBit.build_index(datadir, 'b', '0')
+#FastBit.build_index(datadir, 'c', '0')
 
 nerrors = 0
-for i in xrange(0, 5):
-    t = time.time()
-    q = Query('', datadir, conditions[i])
-    nhits = q.get_result_rows()
-    if nhits != counts[i]:
-        print i, 'query "%s" on %d built-in records found %d hits, but %d were expected' % (str(q), nvals, nhits, counts[i])
-        nerrors += 1
-    del(q)
-    print "time to query '%s': %.3f" % (conditions[i], time.time() - t)
+# NOTE runs faster second time.
+for rep in range(2):
+    if rep == 1:
+        print "\nafter indexes (automatically) created...\n"
+    for i in xrange(0, 5):
+        t = time.time()
+        q = Query('', datadir, conditions[i])
+        nhits = q.rows
+        if nhits != counts[i]:
+            print i, 'query "%s" on %d built-in records found %d hits, but %d were expected' % (str(q), nvals, nhits, counts[i])
+            nerrors += 1
+        del(q)
+        print "time to query '%s': %.3f. results: %i" % (conditions[i], time.time() - t, nhits)
 
 print 'built in tests finished with %d errors' % nerrors
+
